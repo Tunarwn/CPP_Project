34ac42f57bd64774a0d81d97ba3e120b5f548e75
Tunahan Turna <96257156+Tunarwn@users.noreply.github.com>
2023-08-23 11:51:05 +0000

    make

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -13,5 +13,9 @@ int main( void ) {
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+
+    // std::cout << via(5) << std::endl;
+    // std::cout << via(5.5) << std::endl;
 return 0;
 }
--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -3,28 +3,38 @@
 
 #include <iostream>
 
-template <typename T>
-void swap(T &a, T &b){
-    T temp;
-    temp = a;
-    a = b;
-    b = temp;
+// template <typename T>
+// void swap(T &a, T &b){
+//     T temp;
+//     temp = a;
+//     a = b;
+//     b = temp;
+// }
+
+// template <typename C>
+// C min(C &min1, C &min2){
+//     if(min1 < min2)
+//         return min1;
+//     else
+//         return min2;
+// }
+
+// template <typename X>
+// X max(X &max1, X &max2){
+//     if(max1 > max2)
+//         return max1;
+//     else
+//         return max2;
+// }
+
+
+void via(int temp){
+    temp * temp;
 }
 
-template <typename C>
-C min(C &min1, C &min2){
-    if(min1 < min2)
-        return min1;
-    else
-        return min2;
-}
 
-template <typename X>
-X max(X &max1, X &max2){
-    if(max1 > max2)
-        return max1;
-    else
-        return max2;
+void via(double temp){
+    temp * temp;
 }
 
 #endif
